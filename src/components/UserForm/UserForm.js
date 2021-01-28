d2b1c70a7332916da8c85d667b642753e8b94b16
Ivan Fesenko <pharmivan@gmail.com>
2021-01-28 16:45:05 +0200

    tabs added

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,15 +1,51 @@
+import { useState } from 'react';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 
 import AuthForm from '../AuthForm';
 import RegistrationForm from '../RegistrationForm';
 
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && children}
+    </div>
+  );
+}
+
 function UserForm() {
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (_, tabNum) => setTab(tabNum);
+
   return (
     <>
       <div>
-        <RegistrationForm />
-        <AuthForm />
+        <Tabs value={tab} onChange={handleChange}>
+          <Tab label="Authorization" {...a11yProps(0)} />
+          <Tab label="Registration" {...a11yProps(1)} />
+        </Tabs>
+        <TabPanel value={tab} index={0}>
+          <AuthForm />
+        </TabPanel>
+        <TabPanel value={tab} index={1}>
+          <RegistrationForm />
+        </TabPanel>
       </div>
     </>
   );
